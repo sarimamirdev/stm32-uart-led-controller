a7251357796c5e8eeeac019c6d305709b88d4089
Muhammad Sarim Amir <148966257+sarimamirdev@users.noreply.github.com>
2026-08-16 22:42:02 +0500

    Add USART header file with initialization function

--- inc/usart.h
+++ inc/usart.h
@@ -1,1 +1,12 @@
+#ifndef __USART_H
+#define __USART_H
 
+#include "stm32f1xx_hal.h"
+
+/* UART Handle Declaration */
+extern UART_HandleTypeDef huart1;
+
+/* Function Prototypes */
+void USART1_Init(void);
+
+#endif /* __USART_H */
