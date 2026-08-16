ea15ee74fe5b61fe788c3eea9d1e4746a2a4b1b0
Muhammad Sarim Amir <148966257+sarimamirdev@users.noreply.github.com>
2026-08-16 22:41:36 +0500

    Implement GPIO initialization function
    This function initializes GPIO pins for LED2 output and USART1 communication.

--- src/gpio.c
+++ src/gpio.c
@@ -1,1 +1,37 @@
+#include "gpio.h"
 
+/**
+ * @brief Initialize GPIO pins
+ * - PE6: LED2 Output (Active Low)
+ * - PA9: USART1 TX
+ * - PA10: USART1 RX
+ */
+void GPIO_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    /* Enable GPIO Clocks */
+    __HAL_RCC_GPIOE_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    /* Configure PE6 - LED2 Output */
+    GPIO_InitStruct.Pin = LED2_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(LED2_PORT, &GPIO_InitStruct);
+    LED2_OFF();  /* LED OFF by default */
+
+    /* Configure PA9 - USART1 TX */
+    GPIO_InitStruct.Pin = GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /* Configure PA10 - USART1 RX */
+    GPIO_InitStruct.Pin = GPIO_PIN_10;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
