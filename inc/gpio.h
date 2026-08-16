5e2a07c4f11c03f75223854c5e15ee47899bbbb1
Muhammad Sarim Amir <148966257+sarimamirdev@users.noreply.github.com>
2026-08-16 22:41:19 +0500

    Add GPIO header file with LED control macros

--- inc/gpio.h
+++ inc/gpio.h
@@ -1,1 +1,18 @@
+#ifndef __GPIO_H
+#define __GPIO_H
 
+#include "stm32f1xx_hal.h"
+
+/* LED2 Pin Definition - Active Low */
+#define LED2_PIN        GPIO_PIN_6
+#define LED2_PORT       GPIOE
+
+/* LED Control Macros */
+#define LED2_ON()       HAL_GPIO_WritePin(LED2_PORT, LED2_PIN, GPIO_PIN_RESET)
+#define LED2_OFF()      HAL_GPIO_WritePin(LED2_PORT, LED2_PIN, GPIO_PIN_SET)
+#define LED2_TOGGLE()   HAL_GPIO_TogglePin(LED2_PORT, LED2_PIN)
+
+/* Function Prototype */
+void GPIO_Init(void);
+
+#endif /* __GPIO_H */
