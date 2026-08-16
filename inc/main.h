2ddab841231d0990b341a533b2fbe32299aa53b7
Muhammad Sarim Amir <148966257+sarimamirdev@users.noreply.github.com>
2026-08-16 22:38:01 +0500

    Add header file main.h for STM32 HAL configuration

--- inc/main.h
+++ inc/main.h
@@ -1,1 +1,24 @@
+#ifndef __MAIN_H
+#define __MAIN_H
 
+#include "stm32f1xx_hal.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+extern UART_HandleTypeDef huart1;
+extern GPIO_InitTypeDef GPIO_InitStruct;
+
+/* Private define ------------------------------------------------------------*/
+#define LED2_Pin GPIO_PIN_6
+#define LED2_GPIO_Port GPIOE
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+void Error_Handler(void);
+
+#endif /* __MAIN_H */
