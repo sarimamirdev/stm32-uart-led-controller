059a0999caeb2fb4a71b051628a25cd5aafaae95
Muhammad Sarim Amir <148966257+sarimamirdev@users.noreply.github.com>
2026-08-16 22:42:25 +0500

    Add USART1 initialization function
    Initialize USART1 with specified configuration parameters.

--- src/usart.c
+++ src/usart.c
@@ -1,1 +1,24 @@
+#include "usart.h"
 
+UART_HandleTypeDef huart1;
+
+/**
+ * @brief Initialize USART1
+ * - Baud Rate: 115200
+ * - 8 Data Bits, 1 Stop Bit, No Parity
+ * - No Hardware Flow Control
+ */
+void USART1_Init(void)
+{
+    huart1.Instance = USART1;
+    huart1.Init.BaudRate = 115200;
+    huart1.Init.WordLength = UART_WORDLENGTH_8B;
+    huart1.Init.StopBits = UART_STOPBITS_1;
+    huart1.Init.Parity = UART_PARITY_NONE;
+    huart1.Init.Mode = UART_MODE_TX_RX;
+    huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+    
+    if (HAL_UART_Init(&huart1) != HAL_OK)
+        Error_Handler();
+}
