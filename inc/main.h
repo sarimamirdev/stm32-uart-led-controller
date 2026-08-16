7e8d9ed158a9a9518845d026ece4e1cb2892e684
Muhammad Sarim Amir <148966257+sarimamirdev@users.noreply.github.com>
2026-08-16 22:40:36 +0500

    Clean up main.h by removing unused code
    Removed unused includes and private variables from main.h.

--- inc/main.h
+++ inc/main.h
@@ -2,23 +2,11 @@
 #define __MAIN_H
 
 #include "stm32f1xx_hal.h"
+#include <stdio.h>
+#include <string.h>
 
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private variables ---------------------------------------------------------*/
-extern UART_HandleTypeDef huart1;
-extern GPIO_InitTypeDef GPIO_InitStruct;
-
-/* Private define ------------------------------------------------------------*/
-#define LED2_Pin GPIO_PIN_6
-#define LED2_GPIO_Port GPIOE
-
-/* USER CODE BEGIN Private defines */
-
-/* USER CODE END Private defines */
-
+/* System Clock Functions */
+void SystemClock_Config(void);
 void Error_Handler(void);
 
 #endif /* __MAIN_H */
